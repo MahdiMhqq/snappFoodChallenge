bd1ec6c77f26ecc81719cdf3617a622f0d7ddee6
Mahdi Mohaqeq <mahdi.mohaqeq.mm@gmail.com>
2023-07-26 16:26:29 +0330

    [ADD-MM] Icons and favicons + home page styles imporve

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 import styles from "./main.module.scss";
@@ -8,6 +7,15 @@ import Button from "../../components/Button";
 function MainPage() {
   return (
     <main className={styles.main}>
+      <img
+        className={styles.main__logo}
+        src="/snappFoodLogoBig.png"
+        alt={"snappFood"}
+      />
+      <p className={styles.main__desc}>
+        To see main page of code challenge please route to{" "}
+        <code>vendors page</code> by clicking the button below.
+      </p>
       <Link to={"/vendors"}>
         <Button className={styles.main__btn}>دیدن لیست رستوران ها</Button>
       </Link>
